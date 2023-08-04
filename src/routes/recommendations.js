b4d8226767836ed1910a651ceaf3eff591ca9dd4
AaronMcHatton <40744113+AaronMcHatton@users.noreply.github.com>
2023-08-04 13:52:07 -0500

    updating tasting inputs and updating prompt

--- src/routes/recommendations.js
+++ src/routes/recommendations.js
@@ -2,19 +2,19 @@ const express = require('express')
 const recommendationsController = require('../controllers/recommendations.controller.js')
 const recommendations = express.Router()
 
-recommendations.post('/recommend', async (req, res)=>{
-    const rec = await recommendationsController.getRecommendation(req.body.flavors)
+recommendations.post('/tasting', async (req, res)=>{
+    const rec = await recommendationsController.getTastingRecommendation(req.body.flavors)
     res.status(200).send({
         statusCode: 200,
         message: rec
     })
 })
 
-recommendations.post('/image', async (req, res)=>{
-    const img = await recommendationsController.getImage(req.body.prompt)
+recommendations.post('/single', async (req, res)=>{
+    const rec = await recommendationsController.getSingleRecommendation(req.body.flavors)
     res.status(200).send({
         statusCode: 200,
-        message: img
+        message: rec
     })
 })
 
